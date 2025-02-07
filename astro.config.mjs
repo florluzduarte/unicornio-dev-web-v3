2049eeb686876772ecb3ed737564633304607919
Florencia Luz Duarte <florluzduarte@gmail.com>
2025-02-07 20:06:30 -0300

    feat: add Footer, fix navbar, add astro-icon integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,18 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-    },
-    i18n: {
-        locales: ["es", "en"],
-        defaultLocale: "en"
-    }
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  i18n: {
+      locales: ["es", "en"],
+      defaultLocale: "en"
+  },
+
+  integrations: [icon()]
 });
